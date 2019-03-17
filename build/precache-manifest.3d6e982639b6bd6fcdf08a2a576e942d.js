2ed657ac3ad5443a12edcfd7e949bb58c1177ecf
jessicaliu42 <jessicaliurei@gmail.com>
2019-03-16 22:53:12 -0700

    add react app

---
+++ build/precache-manifest.3d6e982639b6bd6fcdf08a2a576e942d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fef2cca846d70c8d8c0d",
+    "url": "/portfolio/static/css/main.37a4c7b8.chunk.css"
+  },
+  {
+    "revision": "fef2cca846d70c8d8c0d",
+    "url": "/portfolio/static/js/main.561b9f07.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/portfolio/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "c64d884169b91a4b469eaf4e40458484",
+    "url": "/portfolio/index.html"
+  }
+];

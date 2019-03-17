56342b4732456da8090894903840e09c0969f980
jessicaliu42 <jessicaliurei@gmail.com>
2019-03-16 22:32:51 -0700

    test it

---
+++ precache-manifest.ffe7e85dc3af93cf125fd0e15f451092.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19ac6d6389375e0d3a86",
+    "url": "/static/css/main.8ae12583.chunk.css"
+  },
+  {
+    "revision": "19ac6d6389375e0d3a86",
+    "url": "/static/js/main.3347705d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "8b65926f67f3289c357deb536f7933a0",
+    "url": "/index.html"
+  }
+];
